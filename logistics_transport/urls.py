3879d60e25f823b1badf4473a3eb9cc662735382
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-09-04 20:15:54 -0300

    feat: register APIRouter Driver

--- logistics_transport/urls.py
+++ logistics_transport/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+
+from apps.driver.views import DriverView
+
+APIRouter = routers.DefaultRouter()
+APIRouter.register(r"drivers", DriverView, basename="Drivers")
 
 urlpatterns = [
+    path("", include(APIRouter.urls)),
     path("admin/", admin.site.urls),
 ]
