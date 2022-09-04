b9b6943bf2d2f8d6e20d526dab7f39cc7238713e
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-09-04 20:07:59 -0300

    feat: created model Driver

--- apps/driver/models.py
+++ apps/driver/models.py
@@ -1,3 +1,27 @@
+import enum
+
 from django.db import models
 
-# Create your models here.
+from apps.driver.exceptions import VehicleTypeInvalid
+
+
+class VehicleTypes(enum.Enum):
+    TRUCK_3_4 = "1"
+    TRUCK_TOCO = "2"
+    TRUCK_TRUCK = "3"
+    TRUCK_SIMPLES = "4"
+    TRUCK_EIXO = "5"
+
+
+class Driver(models.Model):
+    first_name = models.CharField(max_length=100, null=False)
+    last_name = models.CharField(max_length=100, null=False)
+    age = models.IntegerField(null=False)
+    gender = models.CharField(max_length=50, null=False)
+    own_vehicle = models.BooleanField(default=False)
+    driver_license = models.CharField(max_length=5, null=False)
+    loaded = models.BooleanField(default=False)
+    vehicle_types = models.CharField(max_length=50, null=False)
+
+    def __str__(self):
+        return self.first_name
