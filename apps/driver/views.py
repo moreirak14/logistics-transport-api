8bac5435a936e786fe5d9b75ee567096b33b9d16
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-09-05 18:40:24 -0300

    feat: added filter own_vehicle in router driver

--- apps/driver/views.py
+++ apps/driver/views.py
@@ -8,4 +8,9 @@ class DriverView(ModelViewSet):
     serializer_class = DriverSerializer
 
     def get_queryset(self):
+        own_vehicle = self.request.query_params.get("own_vehicle", None)
+
+        if own_vehicle is not None:
+            return Driver.objects.filter(own_vehicle=own_vehicle)
+
         return Driver.objects.all()
