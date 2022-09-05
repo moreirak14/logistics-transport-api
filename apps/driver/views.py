5b7af52fab6dce478df7ce1915428f8b5b64f651
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-09-05 20:27:28 -0300

    feat: added filter loaded in router driver

--- apps/driver/views.py
+++ apps/driver/views.py
@@ -9,8 +9,12 @@ class DriverView(ModelViewSet):
 
     def get_queryset(self):
         own_vehicle = self.request.query_params.get("own_vehicle", None)
+        loaded = self.request.query_params.get("loaded", None)
 
         if own_vehicle is not None:
             return Driver.objects.filter(own_vehicle=own_vehicle)
+
+        if loaded is not None:
+            return Driver.objects.filter(loaded=loaded)
 
         return Driver.objects.all()
