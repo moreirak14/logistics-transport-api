9437decd5e11210efb3898ede3b9f82ea5459365
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-09-05 18:30:11 -0300

    feat: added shipping in DriverSerializer and created driver + shipping

--- apps/driver/serializers.py
+++ apps/driver/serializers.py
@@ -1,15 +1,41 @@
 from rest_framework import serializers
 
 from apps.driver.models import Driver
+from apps.shipping.serializers import ShippingSerializer
 
 
 class DriverSerializer(serializers.ModelSerializer):
+    shipping = ShippingSerializer(many=True)
+
     class Meta:
         model = Driver
-        fields = "__all__"
+        fields = [
+            "id",
+            "first_name",
+            "last_name",
+            "age",
+            "gender",
+            "own_vehicle",
+            "driver_license",
+            "loaded",
+            "vehicle_types",
+            "shipping",
+        ]
 
-    def create(self, validated_data):
+    def _created_shipping(self, shipping, driver):
+        serializer = ShippingSerializer()
+        for data in shipping:
+            shipping_full = serializer.create(validated_data=data)
+            driver.shipping.add(shipping_full)
+
+    def create(self, validated_data) -> Driver:
+        shipping = validated_data["shipping"]
+        del validated_data["shipping"]
+
         if vehicle_types := validated_data["vehicle_types"]:
             Driver.validate_vehicle_types(vehicle_types=vehicle_types)
+
         driver = Driver.objects.create(**validated_data)
+        self._created_shipping(shipping=shipping, driver=driver)
+
         return driver
