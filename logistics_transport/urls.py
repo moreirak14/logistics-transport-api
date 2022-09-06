a19e228cf7ab12bec494ae7baa2844c99da5bd98
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-09-06 14:08:17 -0300

    feat: poetry added drf-yasg (swagger) and config swagger

--- logistics_transport/urls.py
+++ logistics_transport/urls.py
@@ -14,11 +14,26 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
-from rest_framework import routers
+from django.urls import include, path, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions, routers
 
 from apps.driver.views import DriverView
 from apps.shipping.views import ShippingView
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 APIRouter = routers.DefaultRouter()
 APIRouter.register(r"drivers", DriverView, basename="Drivers")
@@ -27,4 +42,10 @@ APIRouter.register(r"shipping", ShippingView, basename="Shipping")
 urlpatterns = [
     path("", include(APIRouter.urls)),
     path("admin/", admin.site.urls),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(
+       cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
+     name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
+     name='schema-redoc'),
 ]
