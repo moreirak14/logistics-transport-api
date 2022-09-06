2e5d9d6fb4d5363bb18459adea2822a71af2e047
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-09-06 19:28:27 -0300

    fix: refactor create driver

--- apps/driver/serializers.py
+++ apps/driver/serializers.py
@@ -18,8 +18,3 @@ class DriverSerializer(serializers.ModelSerializer):
             "vehicle_types",
             "shipping",
         ]
-
-    def create(self, validated_data) -> Driver:
-        if vehicle_types := validated_data["vehicle_types"]:
-            Driver.validate_vehicle_types(vehicle_types=vehicle_types)
-        return Driver.objects.create(**validated_data)
--- apps/driver/views.py
+++ apps/driver/views.py
@@ -1,3 +1,6 @@
+from rest_framework import status
+from rest_framework.request import Request
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from apps.driver.models import Driver
@@ -5,7 +8,22 @@ from apps.driver.serializers import DriverSerializer
 
 
 class DriverView(ModelViewSet):
+    queryset = Driver.objects.all()
     serializer_class = DriverSerializer
+
+    def create(self, request: Request, *args, **kwargs):
+        data = request.data
+        serializer = DriverSerializer(data=data)
+
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        if vehicle_types := data["vehicle_types"]:
+            Driver.validate_vehicle_types(vehicle_types=vehicle_types)
+
+        serializer.save()
+
+        return Response(data=serializer.data, status=status.HTTP_201_CREATED)
 
     def get_queryset(self):
         own_vehicle = self.request.query_params.get("own_vehicle", None)
