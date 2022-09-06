9fe9f112850467ce95ae30976c06a09772a10e92
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-09-06 18:48:49 -0300

    feat: adjusting create driver

--- apps/driver/serializers.py
+++ apps/driver/serializers.py
@@ -1,12 +1,9 @@
 from rest_framework import serializers
 
 from apps.driver.models import Driver
-from apps.shipping.serializers import ShippingSerializer
 
 
 class DriverSerializer(serializers.ModelSerializer):
-    shipping = ShippingSerializer(many=True)
-
     class Meta:
         model = Driver
         fields = [
@@ -22,20 +19,7 @@ class DriverSerializer(serializers.ModelSerializer):
             "shipping",
         ]
 
-    def _created_shipping(self, shipping, driver):
-        serializer = ShippingSerializer()
-        for data in shipping:
-            shipping_full = serializer.create(validated_data=data)
-            driver.shipping.add(shipping_full)
-
     def create(self, validated_data) -> Driver:
-        shipping = validated_data["shipping"]
-        del validated_data["shipping"]
-
         if vehicle_types := validated_data["vehicle_types"]:
             Driver.validate_vehicle_types(vehicle_types=vehicle_types)
-
-        driver = Driver.objects.create(**validated_data)
-        self._created_shipping(shipping=shipping, driver=driver)
-
-        return driver
+        return Driver.objects.create(**validated_data)
