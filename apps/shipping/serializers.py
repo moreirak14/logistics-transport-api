0ddcb6dea16cbd4e9f48143ba0d8de84aa543ad5
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-09-06 18:49:04 -0300

    feat: adjusting create shipping

--- apps/shipping/serializers.py
+++ apps/shipping/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 
 from apps.shipping.models import Destination, Origin, Shipping
-from src.services.geolocation import GeolocationAPI
 
 
 class OriginSerializer(serializers.ModelSerializer):
@@ -48,36 +47,9 @@ class ShippingSerializer(serializers.ModelSerializer):
         model = Shipping
         fields = "__all__"
 
-    def get_geolocation(self, zip_code: str) -> None:
-        service = GeolocationAPI()
-        return service.geolocation(zip_code=zip_code)
-
     def create(self, validated_data) -> Shipping:
-        origin = validated_data["origin"]
-        del validated_data["origin"]
-        destination = validated_data["destination"]
-        del validated_data["destination"]
-
-        shipping = Shipping.objects.create(**validated_data)
-
-        if (origin, destination) is not None:
-            if zip_code := origin["zip_code"]:
-                if not Shipping.validate_zip_code(zip_code=zip_code):
-                    coordinates = self.get_geolocation(zip_code=zip_code)
-                    origin.latitude = coordinates["lat"]
-                    origin.longitude = coordinates["lon"]
-                    shipping_origin = Origin.objects.create(**origin)
-                    shipping.origin = shipping_origin
-
-            if zip_code := destination["zip_code"]:
-                if not Shipping.validate_zip_code(zip_code=zip_code):
-                    coordinates = self.get_geolocation(zip_code=zip_code)
-                    destination.latitude = coordinates["lat"]
-                    destination.longitude = coordinates["lon"]
-                    shipping_destination = Destination.objects.create(
-                        **destination)
-                    shipping.destination = shipping_destination
-
+        origin = Origin.objects.create(**validated_data["origin"])
+        destination = Destination.objects.create(**validated_data["destination"])
+        shipping = Shipping(origin=origin, destination=destination)
         shipping.save()
-
         return shipping
